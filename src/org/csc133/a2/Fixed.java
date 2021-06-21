32781aa1827f1aa09df63afb8b5a900855ac691f
sabishii-bit <47200158+sabishii-bit@users.noreply.github.com>
2021-06-21 07:54:22 -0700

    Delete src directory

--- src/org/csc133/a2/Fixed.java
+++
@@ -1,9 +0,0 @@
-package org.csc133.a2;
-
-// Fixed objects are fixed in place. They can not change their location after they are initially set.
-// Fixed objects currently include skyscrapers and refuelingblimps.
-// TO DO: Add limited accessors to positions to positions for child classes so that their positions can't be accessed and changed
-public class Fixed extends GameObject {
-
-	
-}
